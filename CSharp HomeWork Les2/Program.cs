9d7e90fc153682ffb4c7219286824cfbc4fc9d1c
Olya Osypchuk <kanishua.o@gmail.com>
2022-06-30 19:01:38 +0300

    Edits

--- CSharp HomeWork Les2/Program.cs
+++ CSharp HomeWork Les2/Program.cs
@@ -8,120 +8,110 @@ namespace CSharp_HomeWork_Les2
         //If A > B, result is A+B, if A=B, result is A* B, if A<B, result is A-B.
         static int СalculateNumbers(int a, int b)
         {
-            int result;
-
             if (a > b)
             {
-                result = a + b;
-                Console.WriteLine("A + B = " + result);
+                return a + b;
             }
             else if (a == b)
             {
-                result = a * b;
-                Console.WriteLine("A * B = " + result);
+                return a * b;
             }
             else
             {
-                result = a - b;
-                Console.WriteLine("A - B = " + result);
+                return a - b;
             }
-            return result;
         }
 
         //2.The user enters 2 numbers(X and Y).
         //Determine which quarter the point with coordinates(X, Y) belongs to.
         static int DefineAQuarter(int x, int y)
         {
-            int result;
-
             if (x == 0 | y == 0)
             {
-                result = 0;
-                Console.WriteLine("The point lies on the coordinate axis");
+                return 0;
             }
             else if (x > 0 & y > 0)
             {
-                result = 1;
-                Console.WriteLine("The dot is in a quarter " + result);
+                return 1;
             }
             else if (x > 0 & y < 0)
             {
-                result = 2;
-                Console.WriteLine("The dot is in a quarter " + result);
+                return 2;
             }
             else if (x < 0 & y < 0)
             {
-                result = 3;
-                Console.WriteLine("The dot is in a quarter " + result);
+                return 3;
             }
             else
             {
-                result = 4;
-                Console.WriteLine("The dot is in a quarter " + result);
+                return 4;
             }
-            return result;
         }
 
         //3.The user enters 3 numbers(A, B and C). Print them to the console in ascending order.
-        static void DetermineTheAscendingOrder(int a, int b, int c)
+        static string DetermineTheAscendingOrder(int a, int b, int c)
         {
             if (a == b || a == c || b == c)
             {
-                Console.WriteLine("Numbers equal");
+                return $"";
             }
             else if (a < b & b < c)
             {
-                Console.WriteLine($"{a}, {b}, {c}");
+                return $"{a}, {b}, {c}";
             }
             else if (a < c & c < b)
             {
-                Console.WriteLine($"{a}, {c}, {b}");
+                return $"{a}, {c}, {b}";
             }
             else if (b < a & a < c)
             {
-                Console.WriteLine($"{b}, {a}, {c}");
+                return $"{b}, {a}, {c}";
             }
             else if (b < c & c < a)
             {
-                Console.WriteLine($"{b}, {c}, {a}");
+                return $"{b}, {c}, {a}";
             }
             else if (c < b & b < a)
             {
-                Console.WriteLine($"{c}, {b}, {a}");
+                return $"{c}, {b}, {a}";
+            }
+            else if (c < a & c <b)
+            {
+                return $"{c}, {a}, {b}";
             }
             else
             {
-                Console.WriteLine($"{c}, {a}, {b}");
+                return $"";
             }
         }
-
+        
         //4.The user enters 3 numbers (A, B and C).
         //Print to the console the solution(X-values) of the standard form quadratic equation, where AX^2+BX+C=0.
-        static void SolveQuadraticEquation(int a, int b, int c)
+        static string SolveQuadraticEquation(int a, int b, int c)
         {
-            int discriminant;
-            if (a == 0)
+            int discriminant = b * b - 4 * a * c;
+            if (discriminant < 0)
             {
-                Console.WriteLine("The equation cannot be solved, because A equal zero.");
+                return "";
+            }
+            else if (discriminant == 0)
+            {
+                int x = ((-b) + (int)Math.Sqrt(discriminant)) / (2 * a);
+                return $"{x}";
+            }
+            else if (discriminant > 0)
+            {
+                int x1 = ((-b) + (int)Math.Sqrt(discriminant)) / (2 * a);
+                int x2 = ((-b) - (int)Math.Sqrt(discriminant)) / (2 * a);
+                return $"{x1}, {x2}";
+            }
+            else if (a==0)
+            {
+                return "";
             }
             else
             {
-                discriminant = b * b - 4 * a * c;
-                if (discriminant < 0)
-                {
-                    Console.WriteLine("No roots, because the Discriminant equal zero.");
-                }
-                else if (discriminant == 0)
-                {
-                    double x = ((-b) + Math.Sqrt(discriminant)) / (2 * a);
-                    Console.WriteLine($"Root equals {x}");
-                }
-                else
-                {
-                    double x1 = ((-b) + Math.Sqrt(discriminant)) / (2 * a);
-                    double x2 = ((-b) - Math.Sqrt(discriminant)) / (2 * a);
-                    Console.WriteLine($"Roots equals {x1} and {x2}");
-                }
+                return "";
             }
         }
 
@@ -238,32 +228,32 @@ namespace CSharp_HomeWork_Les2
 
         static void Main(string[] args)
         {
-            СalculateNumbers(5, 10);
-            СalculateNumbers(11, 8);
-            СalculateNumbers(3, 3);
+            Console.WriteLine(СalculateNumbers(5, 10));
+            Console.WriteLine(СalculateNumbers(11, 8));
+            Console.WriteLine(СalculateNumbers(3, 3));
             Console.WriteLine("");
 
-            DefineAQuarter(4, 6);
-            DefineAQuarter(4, -6);
-            DefineAQuarter(-4, -6);
-            DefineAQuarter(-4, 6);
-            DefineAQuarter(0, 6);
-            DefineAQuarter(4, 0);
-            DefineAQuarter(0, 0);
+            Console.WriteLine(DefineAQuarter(4, 6));
+            Console.WriteLine(DefineAQuarter(4, -6));
+            Console.WriteLine(DefineAQuarter(-4, -6));
+            Console.WriteLine(DefineAQuarter(-4, 6));
+            Console.WriteLine(DefineAQuarter(0, 6));
+            Console.WriteLine(DefineAQuarter(4, 0));
+            Console.WriteLine(DefineAQuarter(0, 0));
             Console.WriteLine("");
 
-            DetermineTheAscendingOrder(1, 1, 1);
-            DetermineTheAscendingOrder(1, 2, 3);
-            DetermineTheAscendingOrder(1, 3, 2);
-            DetermineTheAscendingOrder(2, 1, 3);
-            DetermineTheAscendingOrder(2, 3, 1);
-            DetermineTheAscendingOrder(3, 1, 2);
-            DetermineTheAscendingOrder(3, 2, 1);
+            Console.WriteLine(DetermineTheAscendingOrder(1, 1, 1));
+            Console.WriteLine(DetermineTheAscendingOrder(1, 2, 3));
+            Console.WriteLine(DetermineTheAscendingOrder(1, 3, 2));
+            Console.WriteLine(DetermineTheAscendingOrder(2, 1, 3));
+            Console.WriteLine(DetermineTheAscendingOrder(2, 3, 1));
+            Console.WriteLine(DetermineTheAscendingOrder(3, 1, 2));
+            Console.WriteLine(DetermineTheAscendingOrder(3, 2, 1));
             Console.WriteLine("");
 
-            SolveQuadraticEquation(0, 3, 4);
-            SolveQuadraticEquation(-1, 2, 3);
-            SolveQuadraticEquation(1, 2, 3);
+            Console.WriteLine(SolveQuadraticEquation(0, 3, 4));
+            Console.WriteLine(SolveQuadraticEquation(-1, 2, 3));
+            Console.WriteLine(SolveQuadraticEquation(1, 2, 3));
             Console.WriteLine("");
 
             FormatNumberToString(5);
